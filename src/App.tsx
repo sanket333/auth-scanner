632e9637446e82f31b08245042b00d1a73af99c8
sanket-behera <sanket.behera@truecaller.com>
2024-09-26 13:31:25 +0530

    integrate apis

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
 import './App.css'
 import QRCode from 'react-qr-code';
-// import ReactPolling from "react-polling/lib/ReactPolling";
+import ReactPolling from "react-polling/lib/ReactPolling";
 import { useEffect, useState } from 'react';
 function App() {
-  const [userData, setUserData] = useState('');
+  const [userData, setUserData] = useState({
+    device_code: "", // UUID
+    user_code: "", // String, randomly generated
+    verification_uri: "", // Deeplink
+    verification_uri_complete: 
+        "", // Deeplink + user code
+    expires_in: "", // Default
+    interval: "" // Default
+  });
+  const [isRejected, SetIsRejected] = useState(false);
+  const [clientData, setClientData] = useState({
+    given_name: ""
+  });
+  const [isPolled, setIsPolled] = useState(true)
   
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const response = await fetch('https://sdk-apps-noneu.truecaller.com/oauth/device/code');
         const result = await response.json();
         console.log(result)
-        setUserData(result[0].name);
+        setUserData(result);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -20,11 +33,18 @@ function App() {
     // Call fetchData when component mounts
     fetchData();
   }, [])
-  // const handleSuccess = (res) => {
-  //   console.log(res);
-  //   // setData(res);
-  //   return false;
-  // };
+  const handleSuccess = (res) => {
+    console.log(res);
+    if(res.status === "REJECTED")   {
+      SetIsRejected(true);
+    }
+    if(res.sub) {
+      setClientData(res)
+      setIsPolled(false);
+    }
+    // setData(res);
+    return false;
+  };
   // const fetchData = async () => {
   //   const data = await fetch("https://jsonplaceholder.typicode.com/users")
   //   const jsonData = await data.json();
@@ -39,36 +59,38 @@ function App() {
           <img width={100} src='../public/logo.jpg'/>
         </section>
         <section className='scanner-container'>
-            <h3>Login With Truecaller</h3>
-            <h2>{userData}</h2>
-            <div
-            style={{
-              height: "auto",
-              width: "100%",
-            }}
-          >
-            <QRCode
-              size={256}
-              style={{ height: "auto", maxWidth: "100%", width: "100%" }}
-              value={"https://www.google.com"}
-              viewBox={`0 0 256 256`}
-            />
-            {/* <ReactPolling
-              url={"https://jsonplaceholder.typicode.com/users"}
-              interval={3000} // in milliseconds(ms)
-              retryCount={10} // this is optional
-              // onSuccess={(res) => console.log(res)}
-              onFailure={() => console.log("handle failure")} // this is optional
-              method={"POST"}
-              render={({ isPolling }) => {
-                if (isPolling) {
-                  return <div> Hello I am polling</div>;
-                } else {
-                  return <div> Hello I stopped polling</div>;
-                }
+          {
+            isPolled ? <div>
+              <h3>Login With Truecaller</h3>
+              {/* <h2>{userData.}</h2> */}
+              <div
+              style={{
+                height: "auto",
+                width: "100%",
               }}
-          /> */}
-          </div>
+            >
+              <QRCode
+                size={256}
+                style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+                value={"https://www.google.com"}
+                viewBox={`0 0 256 256`}
+              />
+              <ReactPolling
+              
+                url={`https://sdk-apps-noneu.truecaller.com/oauth/device/token?grant_type=urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Adevice_code
+                    &device_code=${userData.device_code}`}
+                interval={3000} // in milliseconds(ms)
+                onSuccess={(res) => handleSuccess(res)}
+                onFailure={() => console.log("handle failure")} // this is optional
+                method={"POST"}
+
+              
+              />
+              {isRejected && <div>Sorry... something went wrong.</div>}
+            </div></div> : <div> <h2>Welcome {clientData.given_name}</h2></div>
+          }
+          
+          
         </section>
       </div>
     </>
